55af95c43f15f1a07438ac54b6d15381afd7c054
Mahmoud Hammad <hodaman2012@gmail.com>
2020-06-13 21:00:23 +0200

    add suncalc to get sun's relative position

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 /*
-1- get coordinates
+1- get coordinates [X]
 2- calculate dirction ****************
 3- display the shadow in the ui as 2D
 4- display the shadow in the ui as 3D
 */
+let post = SunCalc.getPosition(new Date() , 30,30)
+console.log(post)
+
+
 function getLocation() {
   return new Promise((resovle, reject) => {
     let coord = {};
