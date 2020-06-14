2dbb953412be1cd45bc698f6d9b94d5960f9b51b
Mahmoud Hammad <hodaman2012@gmail.com>
2020-06-14 10:33:22 +0200

    change the hour with a slider-> apply changes

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const shadowContainer = document.querySelector(".shadow")
 const RealShadow = document.querySelector(".real-shadow")
+const slider = document.querySelector(".slider")
+slider.oninput = function(){
+  console.log(this.value)
+  morning.setHours(this.value)
+  console.log(morning)
+  drawShadow({x:30,y:30},morning )  
+}
 const userHeight = 200
-let morning = new Date(2020,6,13,14,0,0,0)
-
+let morning = new Date()
 
 function getShadow(coord ,  time=new Date()){
   const  {azimuth , altitude} = SunCalc.getPosition(time , coord.x,coord.y)
@@ -14,7 +20,6 @@ function drawShadow(coord ,time){
   const  {shadowLenth , azimuth}=getShadow(coord,time)
   shadowContainer.style.transform=`rotate(${azimuth }rad)`
   RealShadow.style.height = `${shadowLenth}px`
-
 }
 
 function getLocation() {
@@ -31,7 +36,12 @@ function getLocation() {
   });
 }
 
-getLocation().then(loc => {
-  drawShadow(loc , morning)  
-  console.log(loc)
+getLocation().then(loc => {  
+  // availavble golbally
+  var currentHoure = morning.getHours()
+  slider.value=currentHoure
+  drawShadow(loc,morning )  
+}).catch(err=>{
+  console.log(err)
+  drawShadow({x:30,y:30} ,morning)
 });
