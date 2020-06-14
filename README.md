4f5c3b0feb0a79a00939d65d83aefedad088b10a
Mahmoud Hammad <hodaman2012@gmail.com>
2020-06-14 11:29:22 +0200

    display TIme , minutes slider , refactoring

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ determine the direction of sunlight
 ### Todo:
 1. get coordinates - [x]
 1. Get sun's Postion - [x]
-1. calculate dirction - []
-1. display the shadow in the ui as 2D - []
+1. calculate dirction - [x]
+1. display the shadow in the ui as 2D - [x]
+1. change time with a slider - [x]
+1. display a compass on the UI - []
 1. display the shadow in the ui as 3D - []
--- index.js
+++ index.js
@@ -1,27 +1,49 @@
-const shadowContainer = document.querySelector(".shadow")
-const RealShadow = document.querySelector(".real-shadow")
-const slider = document.querySelector(".slider")
-slider.oninput = function(){
-  console.log(this.value)
-  morning.setHours(this.value)
-  console.log(morning)
-  drawShadow({x:30,y:30},morning )  
-}
-const userHeight = 200
-let morning = new Date()
+const shadowContainer = document.querySelector(".shadow");
+const RealShadow = document.querySelector(".real-shadow");
+const hSlider = document.querySelector(".slider-h");
+const mSlider = document.querySelector(".slider-m");
+const timeUI = document.querySelector(".time");
 
-function getShadow(coord ,  time=new Date()){
-  const  {azimuth , altitude} = SunCalc.getPosition(time , coord.x,coord.y)
-  const shadowLenth = userHeight * Math.cos(altitude)
-  return{shadowLenth,azimuth}
+const userHeight = 200; //use it for calculating shadow lenght
+let morning = new Date();
+
+// Slider ------------------------------------->START
+// Slider ------------------------------------->START
+function updateTime() {
+  drawShadow({ x: 30, y: 30 }, morning);
+  timeUI.innerHTML = morning;
 }
 
-function drawShadow(coord ,time){
-  const  {shadowLenth , azimuth}=getShadow(coord,time)
-  shadowContainer.style.transform=`rotate(${azimuth }rad)`
-  RealShadow.style.height = `${shadowLenth}px`
+hSlider.oninput = function() {
+  morning.setHours(this.value);
+  updateTime();
+};
+
+mSlider.oninput = function() {
+  morning.setMinutes(this.value);
+  updateTime();
+};
+// Slider ------------------------------------->END
+// Slider ------------------------------------->END
+
+// Shadow ------------------------------------->START
+// Shadow ------------------------------------->START
+function getShadow(coord, time = new Date()) {
+  const { azimuth, altitude } = SunCalc.getPosition(time, coord.x, coord.y);
+  const shadowLenth = userHeight * Math.cos(altitude);
+  return { shadowLenth, azimuth };
 }
 
+function drawShadow(coord, time) {
+  const { shadowLenth, azimuth } = getShadow(coord, time);
+  shadowContainer.style.transform = `rotate(${azimuth}rad)`;
+  RealShadow.style.height = `${shadowLenth}px`;
+}
+// Shadow ------------------------------------->END
+// Shadow ------------------------------------->END
+
+// Location ------------------------------------->START
+// Location ------------------------------------->START
 function getLocation() {
   return new Promise((resovle, reject) => {
     let coord = {};
@@ -36,12 +58,28 @@ function getLocation() {
   });
 }
 
-getLocation().then(loc => {  
-  // availavble golbally
-  var currentHoure = morning.getHours()
-  slider.value=currentHoure
-  drawShadow(loc,morning )  
-}).catch(err=>{
-  console.log(err)
-  drawShadow({x:30,y:30} ,morning)
-});
+function useLocation() {
+  getLocation()
+    .then(loc => {
+      const currentHoure = morning.getHours();
+      const currentMinute = morning.getMinutes();
+      hSlider.value = currentHoure;
+      mSlider.value = currentMinute;
+      drawShadow(loc, morning);
+    })
+    .catch(err => {
+      console.log(err);
+      // Failed to get location -> use (30,30) for EGYPT
+      //XXX Pick coordinates from a map later XXX
+      drawShadow({ x: 30, y: 30 }, morning);
+    });
+}
+// Location ------------------------------------->END
+// Location ------------------------------------->END
+
+function init() {
+  timeUI.setAttribute("datetime", morning);
+  timeUI.innerHTML = morning;
+  useLocation();
+}
+init();
