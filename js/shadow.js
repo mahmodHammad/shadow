26142ed870ecbf8ad1ef988acc721ead4eaae915
Mahmoud Hammad <hodaman2012@gmail.com>
2020-06-20 01:30:36 +0200

    init sun

--- js/shadow.js
+++ js/shadow.js
@@ -3,8 +3,8 @@ const illumination = new THREE.AmbientLight(0x101010);
 scene.add(illumination);
 
 // sun
-const light = new THREE.PointLight(0xffffee, 1, 100, 3);
-light.position.set(5, 20, 7.5);
+const light = new THREE.PointLight(0xffffee, 40, 100, 2);
+light.position.set(45, 95, 7.5);
 light.castShadow = true;
 
 light.shadow.mapSize.width = 512; // default
@@ -13,13 +13,6 @@ light.shadow.camera.near = 0.5; // default
 light.shadow.camera.far = 500; // default
 
 scene.add(light);
-
-
-
-const sun = new THREE.PointLight(0xffffee, 0.5, 100, 1);
-sun.position.set(5, 20, 7.5);
-sun.castShadow = true;
-scene.add(sun);
 
 
 
