8800d27a1c64d68dac1a7e52b923ca0e212bef83
Mahmoud Hammad <hodaman2012@gmail.com>
2020-07-05 20:59:28 +0200

    store all movale parts on variables

--- sun/js/model.js
+++ sun/js/model.js
@@ -30,25 +30,41 @@ function createModel() {
       const body = gltf.scene.children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0]
       
       const upperBody = body.children[0].children[0]
-
-      const leg_l = body.children[1]
-      const leg_r = body.children[2]
-      
       const neck = upperBody.children[0]
       const head = neck.children[0]
       const head_sensor = head.children[0]
 
+      const leg_l = body.children[1]
+      const knee_l =  leg_l.children[0]
+      const shin_l =  knee_l.children[0]
+      const foot_l = shin_l.children[0]
+      const toe_l = foot_l.children[0].children[0]
+      console.log("toe",toe_l)
+
+      const leg_r = body.children[2]
+      const knee_r =  leg_r.children[0]
+      const shin_r =  knee_r.children[0]
+      const foot_r = shin_r.children[0]
+      const toe_r = foot_r.children[0].children[0]
+      console.log("Rtoe",toe_r)
+
       const arm_l = upperBody.children[1]
       const arm_l_bi = arm_l.children[0]
       const arm_l_elbow = arm_l_bi.children[0]
+      const forearm_l = arm_l_elbow.children[0]
+      const hand_l = forearm_l.children[0].children[0]
+console.log(hand_l,"hand")
 
       const arm_r = upperBody.children[2]
       const arm_r_bi = arm_r.children[0]
       const arm_r_elbow = arm_r_bi.children[0]
+      const forearm_r = arm_r_elbow.children[0]
+      const hand_r = forearm_r.children[0].children[0]
 
+      console.log(hand_r,"RRRRRRRRRRRRrhand")
 
       
-      const sensor = upperBody.children[0]
+      
 
    
       // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
