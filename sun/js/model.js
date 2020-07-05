018faa025d6374740e730bd20b5e9ee002416df3
Mahmoud Hammad <hodaman2012@gmail.com>
2020-07-05 14:07:23 +0200

    fixing the right toe - run animation function

--- sun/js/model.js
+++ sun/js/model.js
@@ -2,14 +2,33 @@ import { GLTFLoader } from "../assets/GLTFLoader.js";
 import { scene, render } from "./setup.js";
 
 var modelLoader = new GLTFLoader();
+const upperHalf = "spine1_34"
+const neck = "neck_7"
+const rightArm = "collarR_31"
+let test = "hand1L_16"
+test =  "thumb1L_15"
+test =  "thumb2L_14"
+test =  "thumb3L_13"
+const UpperHalfLeg =  "hipL_38"
+const LeftKnee =  "kneeL_37"
+test = "kneeL_37"
+test =  "toeL_41"
+test =  "footL_35"
+test =  "shinL_36"
+test =  "hand2L_12"
+test ="hand3L_11"
+const leftArm = "collarL_19"
+const leftHand = "handIKL_17"
+
 
 function createModel() {
   modelLoader.load(
     "./assets/model/scene.gltf",
     function (gltf) {
+      console.log(gltf.parser.json.nodes[0].rotation[0]=Math.PI/2)
       gltf.scene.traverse(function (node) {
         if (node.type == "Bone") {
-          console.log(node.name);
+          console.log(node);
         }
         // node.scale.x=1.1
         // node.scale.y=1.1
@@ -18,29 +37,12 @@ function createModel() {
         // node.rotation.z = -Math.PI / 20;
         // node.rotation.y = -Math.PI / 40;
 
-       const upperHalf = "spine1_34"
-       const neck = "neck_7"
-       const leftArm = "collarL_19"
-       const rightArm = "collarR_31"
-       const leftHand = "handIKL_17"
-       let test = "hand1L_16"
-       test ="hand3L_11"
-      test =  "hand2L_12"
-      test =  "thumb1L_15"
-      test =  "thumb2L_14"
-      test =  "thumb3L_13"
-      test =  "sensorslookTo_32"
-      const UpperHalfLeg =  "hipL_38"
-      const LeftKnee =  "kneeL_37"
-      test = "kneeL_37"
-      test =  "toeL_41"
-      test =  "footL_35"
-      test =  "shinL_36"
-      if (node.name == LeftKnee ) {
-        node.rotation.x += Math.PI / 8;
-      
+ 
+      if (node.name == leftArm ) {
+        node.rotation.x += Math.PI / 2;
       }
-         if (node.name == UpperHalfLeg ) {
+
+         if (node.name == UpperHalfLeg) {
           console.log("hey you ", node);
           // node.scale.x=2
           // node.scale.y=2
@@ -51,15 +53,7 @@ function createModel() {
           // node.position.x=10
           // node.position.x=0
 
-          let direction = 1
-          setInterval(() => {
-            // node.position.y += 0.02;
-            if(node.rotation.x >Math.PI || node.rotation.x <Math.PI/2){
-              direction *=-1
-            }
-            node.rotation.x += direction*Math.PI/50;
-            render()
-          }, 50);
+          // runAnimation(node)
 
           // node.position.z=1
         }
@@ -77,4 +71,23 @@ function createModel() {
   );
 }
 
+function moveLef(node){
+  // if(node.rotation.x >Math.PI || node.rotation.x <Math.PI/2){
+  //   direction *=-1
+  // }
+  node.rotation.x +=Math.PI/50;
+}
+
+function runAnimation (node1,node2){
+  
+  setInterval(() => {
+    // node.position.y += 0.02;
+    // let direction = 1
+    // node.For
+    //  moveLef(node1)
+    //  moveLef(node2)
+    render()
+  }, 100);
+}
+runAnimation()
 export { createModel };
